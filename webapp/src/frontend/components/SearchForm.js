c310a4803035ee5ac5f276804304daad3c046362
Jari Bakken <jari@holderdeord.no>
2015-02-11 22:46:28 +0100

    Simplify SearchForm

--- webapp/src/frontend/components/SearchForm.js
+++ webapp/src/frontend/components/SearchForm.js
@@ -3,9 +3,18 @@ import SearchAppDispatcher from '../dispatcher/SearchAppDispatcher';
 import TranscriptStore     from '../stores/TranscriptStore';
 import ActionTypes         from '../constants/ActionTypes';
 
-var {div,input,select,option,form} = React.DOM;
+var {div,input,select,option} = React.DOM;
 
-class SearchInput extends React.Component {
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {interval: '24w', query: ''};
+    }
+
+    reset() {
+        this.setState({query: ''});
+    }
+
     componentDidMount() {
         TranscriptStore.addChangeListener(this.reset.bind(this));
     }
@@ -14,36 +23,35 @@ class SearchInput extends React.Component {
         TranscriptStore.removeChangeListener(this.reset.bind(this));
     }
 
-    reset() {
-        var node = React.findDOMNode(this.refs.query);
-
-        node.value = '';
-        node.focus();
+    render() {
+        return div({className: 'form-horizontal'},
+                   div({className: 'row'},
+                       div({className: 'col-md-8'}, this.getSearchInput()),
+                       div({className: 'col-md-2'}, this.getIntervalSelect()),
+                       div({className: 'col-md-2'}, this.getButtons())
+                      )
+                  );
     }
 
-    render() {
+    getSearchInput() {
         return input({
             type: 'search',
             className: 'form-control input-lg',
             name: 'query',
-            ref: 'query',
             autoFocus: true,
             placeholder: 'Ord eller eksakt frase',
-            tabIndex: 0
+            tabIndex: 0,
+            value: this.state.query,
+            onChange: this.handleQueryChange.bind(this),
+            onKeyDown: this.handleKeyDown.bind(this)
         });
     }
-}
 
-class IntervalSelector extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
+    getIntervalSelect() {
         return select({ className: 'form-control input-xs',
                         name: 'interval',
-                        defaultValue: this.props.initialInterval,
-                        onChange: this.handleChange
+                        value: this.state.interval,
+                        onChange: this.handleIntervalChange.bind(this)
                       },
                       option({value: 'month'}, '1 måned'),
                       option({value: '12w'}, '3 måneder'),
@@ -52,58 +60,52 @@ class IntervalSelector extends React.Component {
                      );
     }
 
-    handleChange(e) {
-        this.setState({value: e.target.value });
-    }
-}
-
-class Buttons extends React.Component {
-    render() {
+    getButtons() {
         return div({className: 'form-group'},
                    input({
                        type: 'submit',
                        className: 'btn btn-default btn-sm',
-                       value: 'Legg til ord'
+                       value: 'Legg til ord',
+                       onClick: this.handleSearch.bind(this)
                    }),
                    input({
                        type: 'button',
                        className: 'btn btn-default btn-sm',
                        value: 'Nullstill',
-                       onClick: this.emitReset
+                       onClick: this.handleReset.bind(this)
                    })
                   );
     }
 
-    emitReset() {
+    handleSearch() {
+        if (this.state.query.trim().length) {
+            SearchAppDispatcher.handleViewAction({
+                type: ActionTypes.SEARCH,
+                query: this.state.query,
+                interval: this.state.interval
+            });
+        } else {
+            this.handleReset();
+        }
+    }
+
+    handleReset() {
         SearchAppDispatcher.handleViewAction({
             type: ActionTypes.RESET
         });
     }
-}
 
-class SearchForm extends React.Component {
-    render() {
-        return form({className: 'form-horizontal', onSubmit: this.handleSubmit.bind(this), ref: 'form'},
-                    div({className: 'row'},
-                        div({className: 'col-md-8'}, React.createFactory(SearchInput)()),
-                        div({className: 'col-md-2'}, React.createFactory(IntervalSelector)({initialInterval: '24w'})),
-                        div({className: 'col-md-2'}, React.createFactory(Buttons)())
-                       )
-                   );
+    handleIntervalChange(event) {
+        this.setState({interval: event.target.value});
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        var form = React.findDOMNode(this.refs.form);
+    handleQueryChange(event) {
+        this.setState({query: event.target.value});
+    }
 
-        if (form.query.value.trim().length) {
-            SearchAppDispatcher.handleViewAction({
-                type: ActionTypes.SEARCH,
-                query: form.query.value,
-                interval: form.interval.value
-            });
-        } else {
-            SearchAppDispatcher.handleViewAction({type: ActionTypes.RESET});
+    handleKeyDown(event) {
+        if (event.keyCode === 13) {
+            this.handleSearch();
         }
     }
 }
