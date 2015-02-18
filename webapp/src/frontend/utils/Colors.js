cafe12fbb636f8aa5a36704a7d0e339667906a4c
Henrik Granaas-Helmers <henrik@vivaldi.com>
2015-02-18 15:23:48 +0100

    Adding hdo-bootstrap-variables. Importing Roboto Slab from Google Fonts. - Aiming for visual compatibility with blog.holderdeord.no.

--- webapp/src/frontend/utils/Colors.js
+++ webapp/src/frontend/utils/Colors.js
@@ -1,4 +1,4 @@
 module.exports = {
     // TODO: find a more HDOish color pattern
-    pattern: ["#6a3d9a","#1f78b4","#33a02c","#e31a1c","#ff7f00","#b15928"]
+    pattern: ["#606b82","#1f78b4","#33a02c","#e31a1c","#ff7f00","#b15928"]
 };
