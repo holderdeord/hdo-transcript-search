87a85bc07ef43b52184b166debcb41b363d21bf2
Jari Bakken <jari@holderdeord.no>
2015-02-24 09:55:16 +0100

    Add query string to significant_terms script

--- trends/significant_terms.rb
+++ trends/significant_terms.rb
@@ -7,30 +7,39 @@ algorithms = [:jlh, :mutual_information, :chi_square, :gnd]
 
 algorithms.each do |alg|
   response = client.search(index: 'hdo-transcripts', type: 'speech', body: {
-     size: 0,
-     aggregations: {
-        interesting_words_by_year: {
-            date_histogram: {
-                field: "time",
-                interval: "year"
-            },
-            aggregations: {
-                words: {
-                    significant_terms: {
-                        field: "text",
-                        size: 10,
-                        alg => {}
-                    }
-                }
-            }
-        }
-     }
+    size: 0,
+    query: {
+      query_string: {
+        default_field: "text",
+        query: ARGV.first || "*"
+      }
+    },
+    aggregations: {
+       interesting_words: {
+           date_histogram: {
+               field: "time",
+               interval: "year"
+           },
+           aggregations: {
+               words: {
+                   significant_terms: {
+                       field: "text",
+                       size: 10,
+                       alg => {}
+                   }
+               }
+           }
+       }
+    }
   })
 
-  puts alg
+  puts "#### #{alg}"
+  puts
 
-  response['aggregations']['interesting_words_by_year']['buckets'].each do |bucket|
+  response['aggregations']['interesting_words']['buckets'].each do |bucket|
     words = bucket['words']['buckets'].map { |wbucket| wbucket['key'] }
-    puts "\t#{Time.parse(bucket['key_as_string']).strftime("%Y")} | #{words.join(', ')}"
+    puts "\t#{Time.parse(bucket['key_as_string']).strftime("%Y-%m")} | #{words.join(', ')}"
   end
+
+  puts
 end
